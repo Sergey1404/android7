da5caf3c15314fcc42fa63ffe023ffcfcab8f4b1
Sergey1404 <93861834+Sergey1404@users.noreply.github.com>
2022-12-04 12:58:07 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/mydialer/ContactDiffCallback.kt
@@ -0,0 +1,10 @@
+package com.example.mydialer
+
+import androidx.recyclerview.widget.DiffUtil
+class ContactDiffCallback : DiffUtil.ItemCallback<Contact>() {
+    override fun areItemsTheSame(oldItem: Contact, newItem: Contact): Boolean =
+        oldItem == newItem
+
+    override fun areContentsTheSame(oldItem: Contact, newItem: Contact): Boolean =
+        oldItem == newItem
+}
